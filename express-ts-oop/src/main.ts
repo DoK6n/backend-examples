25f0f976d564c0748c8699311159ddb08c7228ee
Dok6n <ehrbs2018@gmail.com>
2023-06-02 01:29:52 +0900

    express-ts-oop

---
+++ express-ts-oop/src/main.ts
@@ -0,0 +1,21 @@
+import { Application, Request, Response } from 'express';
+import express from 'express';
+import bodyParser from 'body-parser';
+import 'dotenv/config';
+
+const app: Application = express();
+const PORT: number = 8001;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', (req: Request, res: Response) => {
+  // console.dir(req, { depth: Infinity });
+  // console.dir(req, { showHidden: true, depth: null });
+
+  res.send('ok');
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
